22dbc9a8bb4eb367df5b091017cc96255e8190e6
jaswinder <jaswinder.wadali@gmail.com>
2017-12-28 17:17:56 +0530

    first commit

---
+++ base/src/main/java/punjabi/base/MainActivity.java
@@ -0,0 +1,18 @@
+package punjabi.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jaswinderwadali on 28/12/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
