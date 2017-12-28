abb2d0fa27855ac8e2b16b5dd634d96a108d5cba
jaswinder <jaswinder.wadali@gmail.com>
2017-12-28 17:40:57 +0530

    rename and  service class define  manifest file

--- base/src/main/java/punjabi/base/BaseApplication.java
+++ base/src/main/java/punjabi/base/BaseApplication.java
@@ -6,7 +6,7 @@ import android.app.Application;
  * Created by jaswinderwadali on 28/12/17.
  */
 
-public class MyApplication extends Application {
+public class BaseApplication extends Application {
 
 
     @Override
